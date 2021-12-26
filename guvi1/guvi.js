92882cd6ad2ca4454b98fe2eb87671ad4128d225
Arthiratho <76589040+Arthiratho@users.noreply.github.com>
2021-12-26 20:39:01 +0530

    Add files via upload

---
+++ guvi1/guvi.js
@@ -0,0 +1,11 @@
+
+// find average
+// using reverse method
+const nums =[80,60,20,100];
+const output = nums.reduce((sum, curr)=>sum+curr, 0);
+
+
+console.log(output/4);
+
+//expected out 65
+//finally got a  answer
